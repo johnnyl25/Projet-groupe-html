1c5a9ff9fbb897d730b45907e32e8f3016231aeb
johnnyl25 <lemesle.johnny@live.fr>
2019-10-02 17:43:30 +0200

    Test

--- Presentation-html/page/index.php
+++
@@ -1,8 +0,0 @@
-<?php include "../include/header.php"; ?>
-
-<section>
-<p>Trouvez des explications pour les balise HTML</p>
-<img src="../img/nuage-de-mots.jpg" alt="image lache un like">
-</section>
-
-<?php include "../include/footer.php"; ?>
