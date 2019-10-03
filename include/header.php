5b2f74ad719cdd044534432a2f4b86aca976c91b
floriandreau <36734324+floriandreau@users.noreply.github.com>
2019-10-03 11:24:23 +0200

    remise en forme css

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" type="text/css" href="../css/test.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">
 <link rel="stylesheet" type="text/css" href="../css/nav.css">
 <link rel="stylesheet" type="text/css" href="../css/scrolling.css">
 <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Nunito+Sans|Poiret+One|Special+Elite&display=swap" rel="stylesheet">
--- page/index.php
+++ page/index.php
@@ -1,8 +1,8 @@
 <?php include "../include/header.php"; ?>
 
-<section class="scroll-left">
+<div class="scroll-left">
     <div></div>
     <p>Trouvez des explications pour les balise HTML</p>
-</section>
+</div>
 
 <?php include "../include/footer.php"; ?>
