60f21ebdb1ba92a0826b54330760c98b1015a503
amour-youcef <amour-youcef@hotmail.com>
2019-10-04 11:25:58 +0200

    maj-7

--- page/section.php
+++ page/section.php
@@ -30,28 +30,24 @@ require "../include/header.php";
              Ceci peut être un message sur un forum, un article de journal ou de magazine, une parution sur un blog, un commentaire d'utilisateur,
               un widget ou gadget interactif, ou tout autre élément de contenu indépendant.
              Ce contenu est prévu pour être distribué ou réutilisé indépendamment (par exemple dans un flux de syndication).</p>
+
+             <h3> aside : l'élément Aparté&lt; aside &gt;</h3>
+
+        <p>L'élément HTML &lt;aside&gt; (en anglais, "aparté") représente une partie d'un document dont le contenu n'a qu'un rapport 
+        indirect avec le contenu principal du document. Les apartés sont fréquemment présents sous la forme d'encadrés ou de boîtes
+         de légende.</p>
+
         </aside>     
         <aside class="exemple">
-        <img class="centre-img" src="../img/article-html.png" alt="Exemple code html">
-        <img src="../img/article-result.png" alt="Résultat du code html">
+
+        <img class="centre-img" src="../img/aside-html.png" alt="Exemple code html">
+        <img src="../img/aside-result.png" alt="Résultat du code html">
+
         </aside>
     </article>
     <hr>
    
 
-<article>
-    <aside class="text">
-        <h3> aside : l'élément Aparté&lt; aside &gt;</h3>
-
-        <p>L'élément HTML &lt;aside&gt; (en anglais, "aparté") représente une partie d'un document dont le contenu n'a qu'un rapport 
-        indirect avec le contenu principal du document. Les apartés sont fréquemment présents sous la forme d'encadrés ou de boîtes
-         de légende.</p>
-    </aside>     
-    <aside class="exemple">
-    <img class="centre-img" src="../img/aside-html.png" alt="Exemple code html">
-    <img src="../img/aside-result.png" alt="Résultat du code html">
-    </aside>
-</article>
 <hr>
 </section>
 
